a386bfcf93b9dc9d48152476fbee08258eaa18c2
pixaful <goteamtomas@gmail.com>
2024-06-12 23:25:28 +0100

    start from scratch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-with-pagination-template.vercel.app',
-  integrations: [mdx(), sitemap(), tailwind(), svelte()]
+	site: 'https://astro-blog-with-pagination-template.vercel.app',
+	integrations: [mdx(), sitemap()],
 });
